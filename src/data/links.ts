a256fb94de8fa6085840208428accdb7bf6fd3dc
Sameer Shoukat <sameershoukat526@gmail.com>
2025-05-05 18:49:30 +0500

    Initial Commit

---
+++ src/data/links.ts
@@ -0,0 +1,11 @@
+export const Links = {
+  github: "https://github.com/aathifzahir",
+  linkedin:
+    "https://www.linkedin.com/in/sameershoukat-fullstack-javascript-developer/",
+  email: "mailto:sameershoukat526@gmail.com",
+  resume:
+    "https://www.dropbox.com/scl/fi/q4xrx79y71qi3i01yffja/Aathif_Zahir_CV.pdf?rlkey=7v6aicpcshu6h1diqyq9cb0p2&st=wbf2xkt8&dl=1",
+  emailonly: "sameershoukat526@gmail.com",
+  location: "Colombo, Sri Lanka",
+  phone: "+92 317 9610447",
+};
