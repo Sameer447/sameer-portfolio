551d5bdc9e50257efbcbaf00fb8dabc246e249ec
Sameer Shoukat <sameershoukat526@gmail.com>
2025-12-02 20:21:21 +0500

    Feat: Change Resume Link

--- src/data/links.ts
+++ src/data/links.ts
@@ -4,7 +4,7 @@ export const Links = {
     "https://www.linkedin.com/in/sameershoukat-fullstack-javascript-developer/",
   email: "mailto:sameershoukat526@gmail.com",
   resume:
-    "https://drive.google.com/file/d/1MZzdXgekmVEeA5OVh1pzUTM1xqkYvG_w/view?usp=drive_link",
+    "https://drive.google.com/file/d/10R_4fJFzsyc0p1gWmYB05rYJL4v6kI_9/view?usp=sharing",
   emailonly: "sameershoukat526@gmail.com",
   location: "Lahore, Pakistan",
   phone: "+92 317 9610447",
